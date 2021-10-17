497fcd8abd3beae862b428c24f5f07e1783bf029
Alexander Dimitrov <adimitrov23@gmail.com>
2021-10-17 19:33:07 +0300

    updated func definition

--- group-B/02-big-o--basic-algorithms/solutions/05-matrix-sum.cpp
+++ group-B/02-big-o--basic-algorithms/solutions/05-matrix-sum.cpp
@@ -5,7 +5,7 @@
  */
 constexpr uint16_t  M   = 3;
 constexpr uint16_t  N   = 4;
-void SumMatrices( const float mat1[][N], const float mat2[][N], uint16_t m, uint16_t n)
+void SumMatrices( const float mat1[][N], const float mat2[][N] )
 {
     for ( uint16_t i = 0; i < m; i++ )
     {
@@ -22,7 +22,7 @@ int main()
     float   mat1[ M ][ N ]  = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
     float   mat2[ M ][ N ]  = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24 };
 
-    SumMatrices( mat1, mat2, M, N );
+    SumMatrices( mat1, mat2 );
 
     return 0;
 }
