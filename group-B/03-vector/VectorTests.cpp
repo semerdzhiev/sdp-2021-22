3c98747add3e536b5cf9202dbb8f8c4b603fefdb
dimbata23 <adimitrov23@gmail.com>
2021-10-25 20:40:58 +0300

    added asserts to vector tests

--- group-B/03-vector/VectorTests.cpp
+++ group-B/03-vector/VectorTests.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <cassert>
 #include "Vector.hpp"
 
 //------------------------------------------------------------------------------
@@ -8,6 +9,8 @@ Vector<int> initVecWithPushBack(size_t count = 5)
     Vector<int> vec;
     for ( size_t i = 1; i <= count; i++ )
         vec.push_back( i );
+
+    assert( vec.size() == count && vec[ 0 ] == 1 );
 
     return vec;
 }
@@ -44,41 +47,55 @@ void testPushBack()
 void testCtors()
 {
     std::cout << "\nctors test:\n";
-    
+
     std::cout << " - default ctor: \t";
     Vector<int> vec;
     std::cout << vec << "\n";
+
+    assert( vec.empty() );
 
     std::cout << " - size ctor: \t\t";
     Vector<int> vec2( 10 );
     std::cout << vec2 << "\n";
 
+    assert( vec2.size() == 10 );
+
     std::cout << " - size + def val ctor: ";
     Vector<int> vec3( 10, 42 );
     std::cout << vec3 << "\n";
+
+    assert( vec3.size() == 10 && vec3[ 9 ] == 42 );
 
     std::cout << " - init list ctor:\t";
     Vector<int> vec4    = { 1, 2, 3, 4, 5 };
     std::cout << vec4 << "\n";
 
+    assert( vec4.size() == 5 && vec4[ 3 ] == 4 );
+
     std::cout << " - cpy ctor: \t\t";
     Vector<int> vec5    = vec4;
     std::cout << vec5 << "\n";
+
+    assert( vec5.size() == 5 && vec5[ 3 ] == 4 );
 
     std::cout << " - operator=: \t\t";
     Vector<int> vec6;
     vec6    = vec5;
     std::cout << vec6 << "\n";
+
+    assert( vec6.size() == 5 && vec6[ 3 ] == 4 );
 }
 
 //------------------------------------------------------------------------------
 void testIter()
 {
     std::cout << "\niter test:\t\t";
-    
+
     Vector<int> vec = initVecWithPushBack();
     for ( int& elem : vec )
         elem    *= 2;
+
+    assert( vec[ 2 ] == 6 );
 
     /**
     * Range-based for loop:
@@ -115,10 +132,10 @@ void testConstness()
     for ( int elem : vec )          elem;   // compiles:        ✓
     for ( const int& elem : vec )   elem;   // compiles:        ✓
 
-    // doesn't compile: ✓
-    //Vector<int>::iterator   it  = vec.begin();
+                                            // doesn't compile: ✓
+                                            //Vector<int>::iterator   it  = vec.begin();
 
-    // compiles: ✓
+                                            // compiles: ✓
     Vector<int>::const_iterator   it  = vec.begin();
     // *it = 10;   // doesn't compile: ✓
 }
@@ -135,24 +152,40 @@ void testIters()
     it  = vec.erase( it );
     std::cout << " - erase 3rd elem:\t" << vec << "\t, capacity: " << vec.capacity() << "\n";
 
+    assert( vec.size() == 9 );
+
     it  = vec.erase( it, it + 5 );
     std::cout << " - erase 5 elems:\t" << vec << "\t\t\t, capacity: " << vec.capacity() << "\n";
+
+    assert( vec.size() == 4 );
 
     vec.pop_back();
     std::cout << " - pop_back:\t\t" << vec << "\t\t\t, capacity: " << vec.capacity() << "\n";
 
+    assert( vec.size() == 3 );
+
     vec.insert( vec.begin() + 1, 42 );
     std::cout << " - insert at ind 1:\t" << vec << "\t\t\t, capacity: " << vec.capacity() << "\n";
+
+    assert( vec.size() == 4 );
 
     vec.shrink_to_fit();
     std::cout << " - shrink_to_fit:\t" << vec << "\t\t\t, capacity: " << vec.capacity() << "\n";
 
+    assert( vec.capacity() == vec.size() );
+
     std::cout << " - empty():\t\t" << (vec.empty() ? "true" : "false") << "\t\t\t\t, capacity: " << vec.capacity() << "\n";
+
+    assert( !vec.empty() );
 
     vec.clear();
     std::cout << " - clear: \t\t" << vec << "\t\t\t\t, capacity: " << vec.capacity() << "\n";
 
+    assert( vec.size() == 0 && vec.capacity() == 0 );
+
     std::cout << " - empty():\t\t" << (vec.empty() ? "true" : "false") << "\t\t\t\t, capacity: " << vec.capacity() << "\n";
+
+    assert( vec.empty() );
 }
 
 //------------------------------------------------------------------------------
@@ -163,15 +196,26 @@ void testRandomAccess()
     Vector<int> vec = initVecWithPushBack( SIZE );
 
     std::cout << " - front():\t\t" << vec.front() << "\n";
+    assert( vec.front() == 1 );
+
     std::cout << " - back():\t\t" << vec.back() << "\n";
+    assert( vec.back() == 250 );
+
     std::cout << " - vec[ 13 ]:\t\t" << vec[ 13 ] << "\n";
-    std::cout << " - at( 13 ) = 42:\t" << (vec.at( 13 ) = -42) << "\n";
+    assert( vec[13] == 14 );
+
+    std::cout << " - at( 13 ) = -42:\t" << (vec.at( 13 ) = -42) << "\n";
+    assert( vec.at(13) == -42 );
+
     std::cout << " - vec[ 13 ]:\t\t" << vec[ 13 ] << "\n";
+    assert( vec[13] == -42 );
 
     try
     {
         std::cout << " - vec.at( SIZE ):\t";
         std::cout << vec.at( SIZE );
+
+        assert( false );    // Unreachable code
     }
     catch ( const std::out_of_range& e )
     {
