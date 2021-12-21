4750f869cb6be6af15ff52c0faaa019509e1015b
dimbata23 <adimitrov23@gmail.com>
2021-12-21 13:04:19 +0200

    tree task solutions added

--- group-B/09-trees/TreesTasks.cpp
+++ group-B/09-trees/TreesTasks.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
 
 // Memory leak detection define and libraries
 #define _CRTDBG_MAP_ALLOC
@@ -25,26 +29,128 @@ void printPreorder( const BTreeNode* root );
 void printPostorder( const BTreeNode* root );
 void eraseTree( BTreeNode*& root );
 
+// Task 4
+void trim( BTreeNode*& root );
+
+// Get node with min property in BST
+BTreeNode* getMinBST( BTreeNode* bstRoot );
+
+// Basic general tree node structure
+struct TreeNode
+{
+    int                     fData;
+    std::vector<TreeNode*>  fChildren;
+};
+
+void printGeneralTree( const TreeNode* root );
+void eraseTree( TreeNode*& root );
+void printGeneralTreeByLevels( const TreeNode* root );
+
+// Task 7
+int treeHeight( const TreeNode* root );
+
+// Task 8
+int branchingFactor( const TreeNode* root );
+
+// Task 9
+void printLevel( const TreeNode* root, unsigned level );
 
 //------------------------------------------------------------------------------
 int main()
 {
-    BTreeNode*   root       = new BTreeNode( 1 );
+    std::cout
+        << "----------------------------------------------\n"
+        << " Binary Tree\n"
+        << "----------------------------------------------\n";
 
-    root->fpLeft            = new BTreeNode( 2 );
-    root->fpRight           = new BTreeNode( 3 );
+    BTreeNode*   btRoot     = new BTreeNode( 1 );
 
-    root->fpLeft->fpLeft    = new BTreeNode( 4 );
-    root->fpLeft->fpRight   = new BTreeNode( 5 );
+    btRoot->fpLeft          = new BTreeNode( 2 );
+    btRoot->fpRight         = new BTreeNode( 3 );
+
+    btRoot->fpLeft->fpLeft  = new BTreeNode( 4 );
+    btRoot->fpLeft->fpRight = new BTreeNode( 5 );
 
     std::cout << "Inorder:\t";
-    printInorder( root );
+    printInorder( btRoot );
     std::cout << "\nPreorder:\t";
-    printPreorder( root );
+    printPreorder( btRoot );
     std::cout << "\nPostorder:\t";
-    printPostorder( root );
+    printPostorder( btRoot );
 
-    eraseTree( root );  // !!!
+    eraseTree( btRoot );    // !!!
+
+
+    std::cout << "\n\n"
+        << "----------------------------------------------\n"
+        << " General Tree\n"
+        << "----------------------------------------------\n";
+
+    TreeNode*    gtRoot     = new TreeNode
+    {
+        5, {
+            new TreeNode{2, { new TreeNode{4, {} }}},
+            new TreeNode{3, { new TreeNode{6, {} }}},
+            new TreeNode{9, {}},
+            new TreeNode{6, {}}
+        }
+    };
+
+    std::cout << "PrintGeneralTree:\t";
+    printGeneralTree( gtRoot );
+    std::cout << '\n';
+
+    std::cout << "Printed by levels:\t";
+    printGeneralTreeByLevels( gtRoot );
+    std::cout << '\n';
+
+    std::cout << "Height:\t\t\t" << treeHeight(gtRoot) << '\n';
+    std::cout << "Level 2:\t\t";
+    printLevel( gtRoot, 2 );
+
+    eraseTree( gtRoot );    // !!!
+
+
+    std::cout << "\n\n"
+        << "----------------------------------------------\n"
+        << " STL Containers\n"
+        << "----------------------------------------------\n";
+
+    // unnamed scope so that containers call their destructors before dumping leaks
+    {
+        using FN_t      = int;
+        using name_t    = std::string;
+
+        std::multimap<FN_t, name_t>   map;
+        for ( size_t i = 0; i < 10; i++ )
+        {
+            map.insert( { rand() % 10, std::string( "Georgi" ) + std::to_string( i ) } );
+        }
+
+        //std::map<FN_t, name_t>::iterator    it  = map.find( 0 );
+        //if ( it != map.end() )
+        //{
+        //    std::pair<FN_t, name_t> res = *it;
+        //    std::cout << res.second;
+        //}
+
+        for ( std::pair<FN_t, name_t> elem : map )
+            std::cout << elem.first << ", " << elem.second << '\n';
+
+        //std::set<int>   set;
+        //for ( size_t i = 0; i < 10; i++ )
+        //{
+        //    set.insert( rand() % 10 );
+        //}
+
+        //for ( int elem : set )            // linear
+        //    std::cout << elem << ' ';     // traversal
+
+        //std::cout << '\n';
+
+        //std::cout << *set.begin() << '\n';    // min elem in constant time
+        //std::cout << *set.rbegin();           // max elem in constant time
+    }
 
     // Detect and dump in the Output window any memory that hasn't been deleted
     _CrtDumpMemoryLeaks();
@@ -98,4 +204,125 @@ void eraseTree( BTreeNode*& root )
     // delete root must be the last operation
     delete root;
     root = nullptr;
+}
+
+void trim( BTreeNode*& root )
+{
+    if ( !root )
+        return;
+
+    if ( root->fpLeft == nullptr && root->fpRight == nullptr )
+    {
+        delete root;
+        root = nullptr;
+    }
+    else
+    {
+        trim( root->fpLeft );
+        trim( root->fpRight );
+    }
+}
+
+BTreeNode* getMinBST( BTreeNode* root )
+{
+    if ( !root )
+        return nullptr;
+
+    if ( root->fpLeft )
+        return getMinBST( root->fpLeft );
+
+    return root;
+}
+
+//------------------------------------------------------------------------------
+void printGeneralTree( const TreeNode* root )
+{
+    if ( !root )
+        return;
+
+    for ( TreeNode* child : root->fChildren )
+        printGeneralTree( child );
+
+    std::cout << root->fData << ' ';
+}
+
+void eraseTree( TreeNode*& root )
+{
+    if ( !root )
+        return;
+
+    for ( TreeNode*& child : root->fChildren )
+        eraseTree( child );
+
+    // delete root must be the last operation
+    delete root;
+    root = nullptr;
+}
+
+void printGeneralTreeByLevels( const TreeNode* root )
+{
+    if ( !root )
+        return;
+
+    std::queue<const TreeNode*>   wave;
+    wave.push( root );
+
+    while ( !wave.empty() )
+    {
+        const TreeNode* curr    = wave.front();
+        wave.pop();
+
+        std::cout << curr->fData << ' ';
+
+        for ( const TreeNode* child : curr->fChildren )
+            wave.push( child );
+    }
+}
+
+int treeHeight( const TreeNode* root )
+{
+    if ( !root )
+        return 0;
+
+    int maxHeight = 0;
+    for ( const TreeNode* node : root->fChildren )
+    {
+        int childHeight = treeHeight( node );
+        if ( childHeight > maxHeight )
+            maxHeight = childHeight;
+    }
+
+    return 1 + maxHeight;
+}
+
+int branchingFactor( const TreeNode* root )
+{
+    if ( !root )
+        return 0;
+
+    int maxBF = root->fChildren.size();
+    for ( const TreeNode* node : root->fChildren )
+    {
+        int childBF = branchingFactor( node );
+        if ( childBF > maxBF )
+            maxBF = childBF;
+    }
+
+    return maxBF;
+}
+
+void printLevel( const TreeNode* root, unsigned level )
+{
+    if ( !root )
+        return;
+
+    if ( level > 0 )
+    {
+        for ( const TreeNode* node : root->fChildren )
+            printLevel( node, level - 1 );
+    }
+    else
+    {
+        std::cout << root->fData << ' ';
+    }
 }
